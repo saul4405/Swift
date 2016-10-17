32ccb3d0a867b2bcea5edb62a6f17e2732d106db
Saul <Saul@sauls-macbook-pro.local>
2016-10-16 23:08:45 -0500

    prueba2

--- pruebaGit/ViewController.swift
+++ pruebaGit/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-//valida cambios
+//valida cambios xx
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
